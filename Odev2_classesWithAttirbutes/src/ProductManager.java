0cd072cfa0deb160622e8d41e42afaf7a64f8437
Fevziurtn <fevziurten@gmail.com>
2022-11-05 10:43:11 +0300

    Java-odev-2
    Inheritance, Classes, Methods. İlerletilmiş konular.

---
+++ Odev2_classesWithAttirbutes/src/ProductManager.java
@@ -0,0 +1,10 @@
+
+public class ProductManager {
+	
+	public void Add(Product product) {
+		//JDBC  ile veritabanına kayıt edilecek İLERİKİ DERSLERDE
+		System.out.println("Ürün eklendi " + product.getName());
+	}
+	
+
+}
